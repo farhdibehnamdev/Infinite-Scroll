adde5cce2ae02acf54c3e0a5784df31dfa4694b9
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-20 12:35:52 +0430

    👔 Added the getImage function for retrieving images with each request.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-const getImages = function () {
-  const images = config.createRequest("/photos/random");
+const getImage = function () {
+  Config.AccessKey = "HGXt8lN7WnaA2chkM1MfxXkLFFHDlxeyBPOSpR2xJM8";
+
+  const config = new Config("photos/random", "Get", {
+    Authorization: Config.AccessKey,
+    "Accept-Version": "v1",
+  });
+  try {
+    const result = config.createRequest();
+    const imageData = result.then((response) => response.json());
+    imageData.then((data) => console.log(data));
+    // console.log(imageData);
+  } catch (error) {
+    console.log(error);
+  }
 };
+getImage();
