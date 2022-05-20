e3506e506d6d60487737736f7c553f7d0ac40dd7
farhdibehnamdev <behnammfarhadi@gmail.com>
2022-05-20 12:39:35 +0430

    👔 Added createRequest function and a convenient way to set the access key.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,26 @@
 class Config {
-  #apiBase = "";
   constructor(endPoint, method, headers, data) {
     this.endPoint = endPoint;
     this.method = method;
     this.headers = headers;
     this.data = data;
   }
-  #createOptions() {
+  #apiBase = "https://api.unsplash.com/";
+  #_key;
+  static set AccessKey(value) {
+    return (this._key = `Client-ID ${value}`);
+  }
+
+  static get AccessKey() {
+    return this._key;
+  }
+  // #accessKey = "Client-ID HGXt8lN7WnaA2chkM1MfxXkLFFHDlxeyBPOSpR2xJM8";
+
+  async createRequest() {
+    const options = this.#createOptions(this.method, this.headers, this.data);
+    return await fetch(this.#apiBase + this.endPoint, options);
+  }
+  #createOptions(method, headers, data) {
     const options = {};
     if (headers) options.headers = headers;
     if (data) options.data = data;
